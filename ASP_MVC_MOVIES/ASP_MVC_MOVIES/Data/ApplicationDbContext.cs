01af4fab3e6d9ce37cd2e0452a4b38028b821960
Mauro Nunes <21815118@bucks.ac.uk>
2021-12-03 10:20:18 +0000

    crude version

---
+++ ASP_MVC_MOVIES/ASP_MVC_MOVIES/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ASP_Razor_Movies.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP_MVC_MOVIES.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ ASP_MVC_MOVIES/ASP_MVC_MOVIES/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_Movies.Models
+{
+    public enum Genres
+    {
+        Thriller, Comedy, Fiction, RomCom, Horror, Action, Drama, Fantasy, Mystery
+    }
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(60), Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public Genres Genre { get; set; }
+
+        [Range(5.99, 14.99), Required]
+        public decimal Price { get; set; }
+    }
+}
+    
